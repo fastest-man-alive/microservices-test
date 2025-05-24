abb97842f96461beac226e07de0928e5bc488ef2
Kaustav Talukdar <talukdark555@gmail.com>
2025-05-24 18:01:53 +0530

    Updated variable name

--- terraform/env/dev/service-account.tf
+++ terraform/env/dev/service-account.tf
@@ -1,6 +1,6 @@
 module "k8s_service_account" {
   source       = "../../modules/service_account"
-  project_id   = var.project
+  project   = var.project
   account_id   = "kubernetes-sa"
   display_name = "Kubernetes Service Account"
   project_roles = [
