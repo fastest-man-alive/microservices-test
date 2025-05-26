15ba46d5a30d104e69c2d85fe69878a7133ba73d
Kaustav Talukdar <talukdark555@gmail.com>
2025-05-26 20:08:55 +0530

    deleting static ip

--- terraform/env/dev/ip_address.tf
+++ terraform/env/dev/ip_address.tf
@@ -1,5 +1,5 @@
-resource "google_compute_address" "gke_ingress_ip" {
+/*resource "google_compute_address" "gke_ingress_ip" {
   name = "gke-ingress-static-ip"
   project = var.project
   region  =var.region
-}
+}*/
